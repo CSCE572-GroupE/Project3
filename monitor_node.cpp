f04fb72ac7e2ea33d911c2980d8bf9b7ef55ff93
TYLER THOMAS SMITH <smithtt3@bender.(none)>
2014-03-24 12:57:46 -0400

    Updated Monitor

--- monitor_node.cpp
+++ monitor_node.cpp
@@ -1,15 +1,27 @@
 #include <ros/ros.h>
-#include "sensor_msgs/Int32"
+#include <std_msgs/Int32.h>
+#include <diagnostic_msgs/DiagnosticArray.h>
+#include <diagnostic_msgs/DiagnosticStatus.h>
 
-void wifiMessageRecieved(sensor_msgs::Int32 wifiStrengthObject){
-	ROS_INFO_STREAM(wifiStrengthObject.data);
+void wifiMessageRecieved(std_msgs::Int32 wifiStrengthObject){
+	ROS_INFO_STREAM("Wifi Signal Strength: " << wifiStrengthObject.data << "%");
+}
+
+void diagnosticMessageRecieved(diagnostic_msgs::DiagnosticArray diagnosticArray){
+	diagnostic_msgs::DiagnosticStatus statusArray[diagnosticArray.size()];
+	statusArray = diagnosticArray.status;
+	for (int i = 0; i < statusArray.size(); i++){
+		diagnostic_msgs::DiagnosticStatus status = statusArray[i];
+		ROS_INFO_STREAM("Diagnostic Type: " << status.level << " MESSAGE: " << status.name << " -- " << status.message);
+	}
 }
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "monitor_node");
-	ros::Nodehandle nh;
+	ros::NodeHandle nh;
 	
-	ros::Subscriber sub = nh.subscribe("wifi_ss", 1000, &wifiMessageRecieved);
-
+	ros::Subscriber wifiSubcriber = nh.subscribe("/wifi_ss", 1000, &wifiMessageRecieved);
+	ros::Subscriber diagnosticSubscriber = nh.subscribe("/diagnostics_toplevel_state", 1000, &diagnosticMessageRecieved);
+	
 	ros::spin();
 }
